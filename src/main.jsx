9564721e089e232384c394b53dea6dc6f6c1d624
muhammadhassan <hassan.affan@gmail.com>
2025-07-17 20:06:19 -0400

    Initial commit for standalone industry4-site

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Industry4Website from './Industry4Website';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Industry4Website />
+  </React.StrictMode>
+);
